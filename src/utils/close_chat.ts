b3f78286428e9a698b0577922c11bd2b595c98e9
Patrick Gimenes <48632980+PatrickGimenes@users.noreply.github.com>
2026-02-23 20:36:56 -0300

    Update close_chats.ts

--- src/utils/close_chat.ts
+++ src/utils/close_chat.ts
@@ -1,12 +1,12 @@
-import zapresponderApi from "@api/zapresponder-api";
+// import zapresponderApi from "@api/zapresponder-api";
 
-export async function closeChat(chatID: string) {
-  const token = process.env.ZAP_API_TOKEN;
-  if (!token) throw new Error("ZAP_API_TOKEN não definido no .env");
+// export async function closeChat(chatID: string) {
+//   const token = process.env.ZAP_API_TOKEN;
+//   if (!token) throw new Error("ZAP_API_TOKEN não definido no .env");
 
-  zapresponderApi.auth(`Bearer ${token}`);
-  zapresponderApi
-    .criaConversaCopy({ chatId: chatID })
-    .then(({ data }) => console.log(data))
-    .catch((err) => console.error(err));
-}
+//   zapresponderApi.auth(`Bearer ${token}`);
+//   zapresponderApi
+//     .criaConversaCopy({ chatId: chatID })
+//     .then(({ data }) => console.log(data))
+//     .catch((err) => console.error(err));
+// }
